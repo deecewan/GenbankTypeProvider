00d25f0b0e2670980ff14bf19a645bf67877cbd9
David Buchan-Swanson <david.buchanswanson@gmail.com>
2018-06-03 11:08:46 +1000

    test nested assembly attachment

--- GenbankTypeProvider.Tests/GenbankTypeProvider.Tests.fs
+++ GenbankTypeProvider.Tests/GenbankTypeProvider.Tests.fs
@@ -1,10 +1,14 @@
-﻿module GenbankTypeProvider.Tests
+﻿(**
+  * Welcome to some of the most brittle tests in history.
+  * These will routinely break every three months, as a new Genbank release is
+  * made.
+  *)
+module GenbankTypeProvider.Tests
 
 open Xunit
-open System.Reflection
 
 [<Fact>]
-let ``Does something`` () =
+let ``Has a Meta property with Author details`` () =
   Assert.Equal("David Buchan-Swanson", Genbank.Provider.Meta.Author)
 
 [<Fact>]
@@ -14,3 +18,17 @@ let ``Gets correct FTP URL for dynamic child`` () =
 [<Fact>]
 let ``Gets correct accession number for arbitarty child`` () =
   Assert.Equal("GCA_000952055.2", Genbank.Provider.``Vertebrate, Mammalian``.Genomes.Aotus_nancymaae.``Annotation Hashes``.``Assembly accession``)
+
+[<Fact>]
+let ``if there is only one assembly, attach directly to genome`` () =
+  Assert.IsType(
+    typeof<string>, 
+    Genbank.Provider.Other.Genomes.``synthetic_bacterium_JCVI-Syn3.0``.``Annotation Hashes``.``Features hash``
+  )
+
+[<Fact>]
+let ``if there is more than on assembly, attach under assembly name`` () =
+  Assert.IsType(
+    typeof<string>,
+    Genbank.Provider.Other.Genomes.enrichment_culture.``GCA_900230615.1_oilsands_bin_006``.``Annotation Hashes``.``Features hash``
+  )
