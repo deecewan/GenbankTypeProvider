1b7f4bdfca82ce3c3d792e268a6ff8879a748259
David Buchan-Swanson <david.buchanswanson@gmail.com>
2018-06-03 16:33:08 +1000

    add a reference file that will see the interactive session

--- GenbankTypeProvider.Tests/Script.fsx
+++ GenbankTypeProvider.Tests/Script.fsx
@@ -1,5 +1,4 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
-let t = Genbank.Provider.Meta.Author
-System.Console.WriteLine("The author is {}", t);
+#r "/Users/david/projects/deecewan/GenbankTypeProvider/GenbankTypeProvider/bin/Debug/GenbankTypeProvider.dll"
