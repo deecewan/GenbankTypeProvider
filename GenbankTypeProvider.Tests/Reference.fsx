665602a035ff685628c9426d04f99dbe4e405076
David Buchan-Swanson <david.buchanswanson@gmail.com>
2018-06-04 16:48:55 +1000

    remove unused reference file

--- GenbankTypeProvider.Tests/Reference.fsx
+++
@@ -1,9 +0,0 @@
-#r "/Users/david/projects/deecewan/GenbankTypeProvider/GenbankTypeProvider/bin/Debug/GenbankTypeProvider.dll"
-
-Genbank.Provider.Protozoa.Genomes.Crithidia_expoeki.``Annotation Hashes``.``Assembly accession`` |> printf "The accession number is %s\n"
-
-Genbank.Provider.Other.Genomes.enrichment_culture.``GCA_900230645.1_oilsands_bin_023``.``Annotation Hashes``.``Assembly accession`` |> printf "The accession number is %s\n"
-Genbank.Provider.Archaea.Genomes.Methanomassiliicoccaceae_archaeon_UBA408.``Annotation Hashes``.``Locations hash``
-Genbank.Provider.Invertebrate.Genomes.Culicoides_sonorensis.``Annotation Hashes``.``Features last changed``
-
-System.Console.Clear();;
