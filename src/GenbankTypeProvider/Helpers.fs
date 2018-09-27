48ca709047febe8f3cc558f298935ce23b918514
David Buchan-Swanson <david.buchanswanson@gmail.com>
2018-09-27 13:45:22 +1000

    remove functionality that doesn't exist cross-platform

--- src/GenbankTypeProvider/Helpers.fs
+++ src/GenbankTypeProvider/Helpers.fs
@@ -1,13 +1,10 @@
 ﻿module GenbankTypeProvider.Helpers
 
 open System
-open System.IO
 open System.Net
 open GenbankTypeProvider
 open System.Collections.Generic
 open System.IO.Compression
-open System.CodeDom.Compiler
-open System.CodeDom
 
 let logger = Logger.createChild(Logger.logger)("Helpers")
 
@@ -64,16 +61,6 @@ let parseGenbankFile (url: string) =
   let res = req.GetResponse() :?> FtpWebResponse
   let stream = new GZipStream(res.GetResponseStream(), CompressionMode.Decompress)
   Bio.IO.GenBank.GenBankParser().Parse(stream)
-
-(**
-  * Converts a string to its literal representation, including and whitespace characters
-  * that might exist, to debug what is actually contained in a printed string
-  *)
-let toLiteral str =
-  use writer = new StringWriter()
-  use provider = CodeDomProvider.CreateProvider("CSharp")
-  provider.GenerateCodeFromExpression(CodePrimitiveExpression(str), writer, null)
-  writer.ToString()
 
 let loadDirectoryFromFTP (item: FTPFileItem) =
   let res = Cache.cache.LoadDirectory(item.location)
